727d0efee3837e78ed1124e12e8a5f5713da4360
JustinLieu06 <justinlieu06@gmail.com>
2019-11-17 16:28:23 -0800

    added addTwoNumbers

--- set.js
+++ set.js
@@ -63,3 +63,41 @@ var reverse = function(x) {
 reverse(123);
 reverse(-123);
 reverse(120);
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+var addTwoNumbers = function(l1, l2) {
+    let node = null;
+    let carry = arguments[2];
+    if (l1 || l2){
+        let val1 = l1? l1.val : 0;
+        let val2 = l2? l2.val : 0;
+        let next1 = l1 ? l1.next : null;
+        let next2 = l2 ? l2.next : null;
+        let val = carry ? val1 + val2 + 1 : val1 + val2
+        node = new ListNode(val%10);
+        node.next = addTwoNumbers(next1, next2, val >= 10);
+    } else if (carry) {
+        node = new ListNode(1);
+        node.next = null;
+    }
+    console.log(node);
+    return node; 
+};
+
+n1 = new ListNode(2);
+n2 = new ListNode(4);
+n3 = new ListNode(3);
+n1.next = n2;
+n2.next = n3;
+
+n4 = new ListNode(5);
+n5 = new ListNode(6);
+n6 = new ListNode(4);
+n4.next = n5;
+n5.next = n6;
+
+addTwoNumbers(n1, n4);
