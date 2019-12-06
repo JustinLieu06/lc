d1effbb6ecfea8b410b1e3c3d815269911ede1b1
JustinLieu06 <justinlieu06@gmail.com>
2019-12-05 22:20:10 -0800

    added maxDepth

--- set2.js
+++ set2.js
@@ -49,3 +49,25 @@ var merge = function(nums1, m, nums2, n) {
 };
 
 merge([1,4,7], 3, [2,3,6], 3);
+
+function TreeNode(val) {
+     this.val = val;
+     this.left = this.right = null;
+ }
+
+var maxDepth = function(root) {
+  if (root === null) return 0;
+  if (root.left === null && root.right === null) return 1;
+  let maxLeft, maxRight;
+  if (root.left) maxLeft = 1 + maxDepth(root.left);
+  if (root.right) maxRight = 1 + maxDepth(root.right);
+  if (maxLeft === undefined) return maxRight;
+  if (maxRight == undefined) return maxLeft;
+  if (maxLeft > maxRight) return maxLeft;
+  else return maxRight;
+};
+
+let root = new TreeNode(1);
+root.right = new TreeNode(2);
+
+console.log(maxDepth(root));
