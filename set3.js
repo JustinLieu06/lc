b49e06f1374a4fe74dd06e845afd7c5c146fc569
JustinLieu06 <justinlieu06@gmail.com>
2019-12-07 23:25:24 -0800

    added levelOrderBottom

--- set3.js
+++ set3.js
@@ -9,3 +9,21 @@ var isSymmetric = function(root) {
   }
   return helper(root.left, root.right);
 };
+
+var levelOrderBottom = function(root) {
+  if (root === null) return [];
+  let ret = [];
+  let queue = [root];
+  while (queue.length){
+      let temp = [];
+      let size = queue.length;
+      for (let i = 0; i < size; i++){
+          let curr = queue.shift();
+          temp.push(curr.val);
+          if (curr.left !== null) queue.push(curr.left);
+          if (curr.right !== null) queue.push(curr.right);
+      }
+      ret.unshift(temp);
+  }
+  return ret;
+};
