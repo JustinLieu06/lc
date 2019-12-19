43a46836405aba3467fde5df593b86b637947d39
JustinLieu06 <justinlieu06@gmail.com>
2019-12-18 17:05:08 -0800

    added pascal's triangle

--- set3.js
+++ set3.js
@@ -40,3 +40,19 @@ var maxProfit = function(prices) {
   }
   return profit;
 };
+
+var generate = function(numRows) {
+  let result = [];
+  for(let i = 1; i <= numRows; ++i) {
+      let arr = [];
+      for(let j = 0; j < i; ++j) {
+          if(j == 0 || j == i - 1) {
+              arr.push(1);
+          } else {
+              arr.push((result[i - 2][j - 1] + result[i - 2][j]));
+          }
+      }
+      result.push(arr);
+  }
+  return result;
+};
