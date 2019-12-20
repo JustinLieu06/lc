51a3529a077ac75faacc655de396dd4059b84113
JustinLieu06 <justinlieu06@gmail.com>
2019-12-19 19:23:10 -0800

    added rob

--- set3.js
+++ set3.js
@@ -75,3 +75,13 @@ var deleteDuplicates = function(head) {
  prev.next = null;
  return head 
 };
+
+var rob = function(nums) {
+  if(nums.length === 0) return 0;
+  if(nums.length === 1) return nums[0];
+  let totals = [nums[0], Math.max(nums[0], nums[1])];
+  for(let i = 2; i < nums.length; i ++){
+      totals[i] = Math.max(totals[i - 1], totals[i - 2] + nums[i]);
+  }
+  return totals[totals.length - 1];
+};
