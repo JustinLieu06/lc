3f61a372ac796c4ace7c9ef670ba69367c6ad0e2
JustinLieu06 <justinlieu06@gmail.com>
2019-12-22 20:48:16 -0800

    added isBalanced

--- set3.js
+++ set3.js
@@ -114,17 +114,15 @@ var maxProfit = function(prices) {
 };
 
 var isBalanced = function(root) {
-    return getHeight(root) !== -1;
-    
-    function getHeight(root) {
-        if(!root) {
-            return 0;
-        }
-        let left = getHeight(root.left);
-        let right = getHeight(root.right);
-        if(left == -1 || right == -1 || Math.abs(left - right) > 1) {
-            return -1;
-        }
-        return 1 + Math.max(left, right);
-    }
+    if (root === null) return true;
+    let lHeight = getHeight(root.left);
+    let rHeight = getHeight(root.right);
+    let res = true;
+    if (Math.abs(lHeight - rHeight) > 1) res = false;
+    return res && isBalanced(root.left) && isBalanced(root.right);
 };
+
+var getHeight = function(node) {
+    if (node === null) return 0;
+    return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+}
