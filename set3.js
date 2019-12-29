0e29fa16ab58d581c766286ff05658633ea3a816
JustinLieu06 <justinlieu06@gmail.com>
2019-12-28 20:51:13 -0800

    added findClosestValueInBst

--- set3.js
+++ set3.js
@@ -219,3 +219,22 @@ function twoNumberSum(array, targetSum) {
 	}
 	return ret;
 }
+
+function findClosestValueInBst(tree, target) {
+      return helper(tree, target, Number.MAX_SAFE_INTEGER);
+  }
+  
+  function helper(tree, target, closest){
+      if (tree === null) return closest;
+      if (Math.abs(target - closest) > Math.abs(target - tree.value))
+          closest = tree.value;
+      if (target < tree.value){
+          return helper(tree.left, target, closest);
+      } 
+      else if (target > tree.value) {
+          return helper(tree.right, target, closest);
+      }
+      else {
+          return closest;
+      }
+  }
