02cc42da919b85654ab7bb70f875ac41f0894097
JustinLieu06 <justinlieu06@gmail.com>
2019-12-29 21:55:54 -0800

    added branchSums

--- set3.js
+++ set3.js
@@ -238,3 +238,28 @@ function findClosestValueInBst(tree, target) {
           return closest;
       }
   }
+
+  class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  function branchSums(root) {
+      let sums = [];
+      helper(root, 0, sums);
+      return sums;
+  }
+
+  function helper(node, runningSum, sums){
+	if (!node) return;
+	let newRunningSum = runningSum + node.value;
+	if (!node.left && !node.right) {
+		sums.push(newRunningSum);
+		return;
+	}
+	helper(node.left, newRunningSum, sums);
+	helper(node.right, newRunningSum, sums);
+}
