c54bac8fd4b751afcebc00d2988defc812a86c90
JustinLieu06 <justinlieu06@gmail.com>
2020-03-30 23:38:26 -0700

    fib

--- set4.js
+++ set4.js
@@ -29,4 +29,9 @@
 
 // console.log(addStrings('35','20'));
 
-console.log(Math.floor(12/10));
+// console.log(Math.floor(12/10));
+function fib(n){
+  if (n === 0 || n == 1) return n;
+  return fib(n-1) + fib(n-2);
+}
+console.log(fib(6));
