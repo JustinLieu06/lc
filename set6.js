fe6a4373bd604e65e0ee48fa5a2a096cd28bbd06
JustinLieu06 <justinlieu06@gmail.com>
2020-05-04 21:47:03 -0700

    spiralTraverse

--- set6.js
+++ set6.js
@@ -223,3 +223,34 @@ function caesarCipherEncryptor(string, key) {
 	}
 	return res;
 }
+
+function spiralTraverse(arr){
+  let res = [];
+  let startRow = 0, endRow = array.length - 1, startCol = 0, endCol = array[0].length - 1;
+  
+  while (startRow <= endRow && startCol <= endCol){
+    for (let col = startCol; col <= endCol; col++){
+      res.push(arr[startRow][col]);
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++){
+      res.push(arr[row[endCol]]);
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--){
+      if (startRow === endRow) break;
+      res.push(arr[endRow][col]);
+    }
+
+    for (let row = endRow - 1; row >= startRow; row--){
+      if (startCol === endCol) break;
+      res.push(arr[row][startCol]);
+    }
+
+    startRow++;
+    endRow--;
+    startCol++;
+    endCol--;
+  }
+  return res;
+}
