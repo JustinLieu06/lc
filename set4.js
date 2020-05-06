8facf5f93cc5d1cd097694543115decb812d26d5
JustinLieu06 <justinlieu06@gmail.com>
2020-05-06 15:56:59 -0700

    isSymmetric

--- set4.js
+++ set4.js
@@ -255,7 +255,16 @@ function threeNumberSum(arr, targetSum) {
 	return resultArr;
 }
 
+function isSymmetric(root){
+  if (!root) return true;
 
+  function isMirror(s, t){
+    if (!s && !t) return true;
+    if (!s || !t || s.val !== t.val) return false;
+    return isMirror(s.left, t.right) && isMirror(s.right, t.left);
+  }
+  return isMirror(root.left, root.right);
+}
 
 
 
