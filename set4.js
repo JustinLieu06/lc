b42fc6025f3961de23c77e4705119fc40f2065b9
JustinLieu06 <justinlieu06@gmail.com>
2020-05-09 00:14:27 -0700

    longest peak

--- set4.js
+++ set4.js
@@ -310,6 +310,31 @@ var toLowerCase = function(str) {
       .join('');
 };
 
+function longestPeak(array){
+  let len = 0;
+  let i = 1;
+  while (i < array.length - 1){
+    const isPeak = array[i - 1] < array[i] && array[i + 1] < array[i];
+    if (!isPeak){
+      i++;
+      continue;
+    }
+
+    let leftIdx = i -2;
+    while(leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]){
+      leftIdx--;
+    }
+    let rightIdx = i + 2;
+    while (rightIdx < array.length && array[rightIdx] < array[rightIdx - 1]){
+      rightIdx++;
+    }
+
+    const currentPeakLen = Math.max(len, currentPeakLen);
+    i = rightIdx;
+  }
+  return len;
+}
+
 
 
 
