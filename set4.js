af56c703caa64288b0c0d776be0738a50d5db4c6
JustinLieu06 <justinlieu06@gmail.com>
2020-05-16 16:44:12 -0700

    calculate

--- set4.js
+++ set4.js
@@ -487,3 +487,14 @@ function branchSums(root){
   calculateBranchSums(root, 0, sums);
   return sums;
 }
+
+function calculateBranchSums(node, runningSum, sums){
+  if (!node) return;
+  const newRunningSum = runningSum + node.value;
+  if (!node.left && !node.right){
+    sums.push(newRunningSum);
+    return;
+  }
+  calculateBranchSums(node.left, newRunningSum, sums);
+  calculateBranchSums(node.right, newRunningSum, sums);
+}
