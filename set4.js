a490b1a377fdd5018a3d84401395a598d034bc62
JustinLieu06 <justinlieu06@gmail.com>
2020-05-19 13:09:00 -0700

    nodeDepths

--- set4.js
+++ set4.js
@@ -500,9 +500,16 @@ function calculateBranchSums(node, runningSum, sums){
 }
 
 function nodeDepths(root){
-  let answer = 0;
-  helper(root, answer);
-  return answer;
+  let sum = 0;
+  const stack = [{node: root, depth: 0}];
+  while (stack.length > 0){
+    const {node, depth} = stack.pop();
+    if (node === null) continue;
+    sum += depth;
+    stack.push({node: node.left, depth: depth + 1});
+    stack.push({node: node.right, depth: depth + 1});
+  }
+  return sum;
 }
 
 function findThreeLargest(array){
